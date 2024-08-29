a5b2d1f4146cffa0a60a8acc9a7f51bd182aa790
Chandandp <chandandp2983@gmail.com>
2024-08-29 23:13:14 +0530

    city

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import CitySelector from './CitySelector';
+
+
+function App() {
+  return (
+    <div>
+      <CitySelector />
+    </div>
+  );
+}
+
+export default App;
